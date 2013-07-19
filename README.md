5685f1f30ee26536839bc76935832a5e913033ef
massyah <massyah@gmail.com>
2013-07-19 14:55:03 +0200

    Updated readme and tracking of R code

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
-PdbMine:Mining the PDB database jointly with MEDLINE
-=======
+PdbMine: Mining the PDB database jointly with MEDLINE
+========================================================
+
+Accompanying code and databases for Article "Timely deposition of macromolecular structures is necessary for peer review", from Robbie P. Joosten, Hayssam Soueidan, Lodewyk Wessels and Anastassis Perrakis. 
 
 # Getting the list of PDB entries 
 
-The PDB webservices are a bit troublesome to navigate in. The easiest way to get every entry with a corresponding citation is to use [advanced seach](http://www.rcsb.org/pdb/search/advSearch.do). 
+The PDB webservices are a bit troublesome to navigate in. The easiest way to get every entry with a corresponding 'citation' (e.g. a MEDLINE indexed article) is to use [advanced seach](http://www.rcsb.org/pdb/search/advSearch.do). 
 
 Select "Citation" as a query type, then check filter to only keep "Primary citation", check the count, generate the results. Once results have been generated, export the table as a "Primary citation report". 
 
@@ -11,11 +13,37 @@ Select "Citation" as a query type, then check filter to only keep "Primary citat
 
 We get them from the PDB ftp site. 
 
-Run pdb-get-multi-thread.py, pointing to the correct list of PDB entries (first lines, CITATIONFILE variable)
+Run [pdb-get-multi-thread.py](pdb-get-multi-thread.py), pointing to the correct list of PDB entries (first lines, CITATIONFILE variable)
 
 Adjust the number of threads available depending on the machine used. 
 
+Clean the pdb-entries folder by removing any empty file:
 
-# Cleaning the data 
+    find . -type f -empty | xargs rm
 
-* Journal names 
+
+# Inserting the entries in a local SQLite3 databases
+Once the entries have been downloaded, we run [medline-get.py](medline-get.py) then :
+
+   pdb_store_entries_not_in_db()
+   get_medlines_for_pdbs() # Takes a while, download all entries from MEDLINE corresponding to PDB entries 
+
+
+
+# Date processing 
+Once entries have been processed, parsed and inserted in the database, we compute the earliestpublication and earliestpublicdate by sourcing [medline-get.py](medline-get.py) and then :
+
+    correct_ahead_of_print()
+    add_earliest_public_date()
+    add_earliest_publication_date()
+
+
+
+Results from these steps are stored in the provided [SQLite database:local-medline-pdb.db](local-medline-pdb.db)
+# Analysis
+All data aggregations and figure generation are performed in R and are described in [deposit_time_analysis.Rmd](deposit_time_analysis.Rmd)
+
+
+# Impact factor table 
+
+For copyright reasons, this table cannot be made available. The impact factor table that we used correponds to the 2013 Thomson Reuters estimate. 
--- pdb-get-multi-thread.py
+++ pdb-get-multi-thread.py
@@ -1,5 +1,8 @@
 import urllib2
 import logging
+import os.path
+import gzip
+import datetime
 import ftplib
 import time
 import os
@@ -37,17 +40,20 @@ if "logger" not in globals():
 
 def ftp_download_entries(pdb_ids):
 	logger.info("Will get %d entries, from %s to %s",len(pdb_ids),sorted(pdb_ids)[0],sorted(pdb_ids)[-1])
+	logger.info("Already present: %d entries",len(set([x.split(".")[0] for x in os.listdir("pdb-entries/")]))
+)
 	ftp = ftplib.FTP("ftp.wwpdb.org")
 	ftp.login("massyah@gmail.com","none")
 	ftp.cwd("/pub/pdb/data/structures/all/pdb")
 	logger.info("Initiated connection, CWD successful")
 
 	success=0
+	tot_entries=len(pdb_ids)
 	for entry in pdb_ids:
 		file=open("pdb-entries/%s.txt.gz"%(entry),"w")
 		ftp.retrbinary("RETR pdb%s.ent.gz"%(entry.lower()),file.write)
 		file.close()
-		logger.info("Downloaded %s",entry)
+		logger.info("Downloaded %s,%d to go",entry,tot_entries-success)
 		success+=1
 	
 
@@ -85,10 +91,12 @@ class Worker(threading.Thread):
 				result = self.function(data)
 				self.out_queue.put(result)
 				self.in_queue.task_done()
-				print "Still",self.in_queue.qsize(),"to do"
+				logger.info("Still %d to do",self.in_queue.qsize())
 			except Exception as e:
-				print "exception in thread:",e
-				return
+				logger.critical('something happened!: Error on %s, %s',repr(data),repr(e))
+				self.out_queue.put({})
+				self.in_queue.task_done()
+				break
 
 def process(data, function, num_workers=1):
 	in_queue = Queue()
@@ -97,6 +105,7 @@ def process(data, function, num_workers=1):
 	out_queue = Queue(maxsize=in_queue.qsize())
 	workers = [Worker(function, in_queue, out_queue) for i in xrange(num_workers)]
 	for worker in workers: 
+		worker.setDaemon(True)
 		worker.start()
 	in_queue.join()
 	return out_queue
@@ -125,11 +134,11 @@ if "all_pdb_ids" not in globals():
 
 
 
-def process_batch_download(nworker=NMAXTHREADS,batch_size=500):
+def process_batch_download(nworker=NMAXTHREADS,batch_size=100,maxN=500):
 	# Filter out the one already downloaded
 	parse_citations()
 	available_entries=set([x.split(".")[0] for x in os.listdir("pdb-entries/")])
-	pdb_ids_to_get=set(all_pdb_ids).difference(available_entries)
+	pdb_ids_to_get=list(set(all_pdb_ids).difference(available_entries))[0:maxN]
 	print "To get",len(pdb_ids_to_get)
 	# Take some pdbid to download, split them in lists 
 
@@ -141,12 +150,50 @@ def process_batch_download(nworker=NMAXTHREADS,batch_size=500):
 
 
 ## Parsing the files in multi-threaded way 
+
+def parse_pdb_entry(pdb_txt):
+	pdb_txt=pdb_txt.split("\n")
+
+	header=[x for x in pdb_txt if x.startswith("HEADER")][0].split()
+	pdbid=header[-1]
+
+	try:
+		depodate=header[-2]
+		depodate=datetime.datetime.strptime(depodate,"%d-%b-%y")# "13-SEP-10" format
+	except ValueError:
+		print >>sys.stderr,"no dep-date for pdb",pdbid
+		depodate=None
+
+	#release date 
+	revdates=[x.split() for x in pdb_txt if x.startswith("REVDAT")]
+	structure_revdate=[x[2] for x in revdates if x[1]=="1"][0]
+	structure_revdate=datetime.datetime.strptime(structure_revdate,"%d-%b-%y")# "13-SEP-10" format
+
+	try:
+		jrnlref=[x.split()[2] for x in pdb_txt if  x.startswith("JRNL") and "PMID" in x][0]
+		jrnlref=int(jrnlref)
+	except IndexError:
+		logger.info("no journal entries for pdb %s",pdbid)
+		jrnlref=None
+
+	return {
+		"pdbid":pdbid,
+		"depodate":depodate,
+		"releasedate":structure_revdate,
+		"jrnlref":jrnlref
+	}
+
 def parse_pdb_file(pdbid):
-	with open("pdb-entries/%s.pdb.txt"%(pdbid),"r") as f:
-		fields=pdb_parse_entry(f.read())
+	if os.path.isfile("pdb-entries/%s.pdb.txt"%(pdbid)):
+		tgt_file = open("pdb-entries/%s.pdb.txt"%(pdbid),"r")
+	elif os.path.isfile("pdb-entries/%s.txt.gz"%(pdbid)):
+		tgt_file=gzip.open("pdb-entries/%s.txt.gz"%(pdbid))
+	fields=parse_pdb_entry(tgt_file.read())
+	tgt_file.close()
 	return fields
 
-def process_batch_pdb_files(nworker=NMAXTHREADS,batch_size=500):
+def process_batch_parse_pdb_files(nworker=NMAXTHREADS,batch_size=500):
+	global conn,cur
 	conn=sqlite3.connect("local-medline-pdb.db")
 	cur=conn.cursor()
 	available_entries=set([x.split(".")[0] for x in os.listdir("pdb-entries/") if (os.path.isfile("pdb-entries/"+x) and not x.startswith("."))])
@@ -154,11 +201,17 @@ def process_batch_pdb_files(nworker=NMAXTHREADS,batch_size=500):
 	pdb_in_db=[x[0] for x in cur.fetchall()]
 	to_insert=set(available_entries).difference(pdb_in_db)
 	print len(to_insert)
+
+
 	batch=sorted(to_insert)[:batch_size]
 	res=process(batch,parse_pdb_file,nworker)
 	res=list(res.queue)
 
+	print "All PDB parsed, inserting into DB",len(res), [x['pdbid'] for x in res]
  	#prepare the data as an array
-	values=[(x['pdbid'],x['depodate'],x['releasedate'],x['jrnlref']) for x in res]
+	values=[(x['pdbid'],x['depodate'],x['releasedate'],x['jrnlref']) for x in res if (len(x)>0) and (x['pdbid'] not in pdb_in_db)]
+	print len(values)
 	cur.executemany("INSERT INTO pdb(pdbid,depodate,releasedate,jrnlref) VALUES(?,?,?,?)",values)
-	conn.commit()
+	print "All insertions finished,commiting"
+
+	return conn.commit()
